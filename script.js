3a6ccd04e0bafb7e2b241855b7db3fbaba7cb15a
iLnsiC <besswalid@gmail.com>
2023-10-31 20:10:07 +0100

    [ADD] Navbar + Responsive ( CSS and JS )

---
+++ script.js
@@ -0,0 +1,7 @@
+const burgerMenuBtn = document.querySelector(".burger-menu-btn");
+const navBarOptions = document.querySelector(".nav-options");
+burgerMenuBtn.addEventListener("click", (e) => {
+  e.preventDefault;
+  e.target.closest(".burger-menu-btn").classList.toggle("open");
+  navBarOptions.classList.toggle("open");
+});
