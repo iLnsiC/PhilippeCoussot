4fd82acc01de01c90e01229c5bf3f33f3a776bc7
iLnsiC <besswalid@gmail.com>
2023-11-19 12:21:39 -0500

    [ADD] Home page v1

--- script.js
+++ script.js
@@ -10,3 +10,49 @@ function toggleSearchBar(state) {
   if (state) {
   }
 }
+
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-btn-next",
+    prevEl: ".swiper-btn-prev",
+  },
+  keyboard: {
+    enabled: true,
+    onlyInViewport: true,
+  },
+
+  breakpoints: {
+    // when window width is >= 320px
+    800: {
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+    // when window width is >= 480px
+    1200: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+    // when window width is >= 640px
+    2000: {
+      slidesPerView: 4,
+      spaceBetween: 40,
+    },
+  },
+});
+
+const swiperLogo = new Swiper(".swiper-logo", {
+  direction: "horizontal",
+  slidesPerView: 3,
+
+  loop: true,
+  autoplay: {
+    delay: 1000,
+    pauseOnMouseEnter: true,
+    waitForTransition: true,
+  },
+});
