41108623a25d5783c3ee1e8d625aa482005b801d
iLnsiC <besswalid@gmail.com>
2023-12-04 22:43:17 +0100

    [WIP] Toggle

--- script.js
+++ script.js
@@ -118,10 +118,17 @@ const swiperExperience = new Swiper(".swiper-experience", {
 });
 
 function filterToggle(e) {
-  const activBtn = document.querySelector(".toggle-options .active");
-  activBtn.classList.remove("active");
+  const activeBtn = document.querySelector(".toggle-options .active");
+  const filteredElement = document.querySelectorAll(".toggle-el")
+  filteredElement.forEach(ele => {
+    if (e.target.dataset.theme === 'all' || e.target.dataset.theme === ele.dataset.theme) {
+      ele.classList.remove("hide")
+    } else if (!ele.classList.contains('hide')) {
+        ele.classList.add("hide")
+    }
+  })
+  activeBtn.classList.remove("active");
   e.target.classList.add("active");
-  console.log(e.target.dataset.theme);
 }
 toggleBtn.forEach((el) => {
   el.addEventListener("click", filterToggle);
